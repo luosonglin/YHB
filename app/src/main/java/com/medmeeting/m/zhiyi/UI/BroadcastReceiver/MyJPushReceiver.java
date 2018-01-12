b323f86dd16052796d59c11cb1af940ac8182396
luosonglin <iluosonglin@qq.com>
2018-01-12 11:16:15 +0800

    feature(点击推送通知栏，跳转对应页)

--- app/src/main/java/com/medmeeting/m/zhiyi/UI/BroadcastReceiver/MyJPushReceiver.java
+++ app/src/main/java/com/medmeeting/m/zhiyi/UI/BroadcastReceiver/MyJPushReceiver.java
@@ -1,6 +1,5 @@
 package com.medmeeting.m.zhiyi.UI.BroadcastReceiver;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -9,7 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.medmeeting.m.zhiyi.R;
+import com.medmeeting.m.zhiyi.UI.IndexView.NewsActivity;
 import com.medmeeting.m.zhiyi.UI.LiveView.LiveProgramDetailActivity2;
 import com.medmeeting.m.zhiyi.UI.MeetingView.MeetingDetailActivity;
 import com.medmeeting.m.zhiyi.UI.VideoView.VideoDetailActivity;
@@ -20,7 +19,6 @@ import org.json.JSONObject;
 
 import java.util.Iterator;
 
-import br.com.goncalves.pugnotification.notification.PugNotification;
 import cn.jpush.android.api.JPushInterface;
 
 public class MyJPushReceiver extends BroadcastReceiver {
@@ -47,21 +45,8 @@ public class MyJPushReceiver extends BroadcastReceiver {
             String content = bundle.getString(JPushInterface.EXTRA_MESSAGE);
             String extra = bundle.getString(JPushInterface.EXTRA_EXTRA);
 
-            String title = bundle.getString(JPushInterface.EXTRA_TITLE);
-            String status = bundle.getString(JPushInterface.EXTRA_STATUS);
-            String notification = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_ACTION_EXTRA);
-            String notification2 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_DEVELOPER_ARG0);
-            String notification3 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_ID);
-            String notification4 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
-
             System.out.println("收到了自定义消息@@消息内容是:" + content);
             System.out.println("收到了自定义消息@@消息extra是:" + extra);
-            System.out.println("收到了自定义消息@@title:" + title);
-            System.out.println("收到了自定义消息@@status:" + status);
-            System.out.println("收到了自定义消息@@notification:" + notification);
-            System.out.println("收到了自定义消息@@notification2:" + notification2);
-            System.out.println("收到了自定义消息@@notification3:" + notification3);
-            System.out.println("收到了自定义消息@@notification4:" + notification4);
 
 
         } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
@@ -70,30 +55,9 @@ public class MyJPushReceiver extends BroadcastReceiver {
             String content = bundle.getString(JPushInterface.EXTRA_MESSAGE);
             String extra = bundle.getString(JPushInterface.EXTRA_EXTRA);
 
-            String title = bundle.getString(JPushInterface.EXTRA_TITLE);
-            String status = bundle.getString(JPushInterface.EXTRA_STATUS);
-            String notification = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_ACTION_EXTRA);
-            String notification2 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_DEVELOPER_ARG0);
-            String notification3 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_ID);
-            String notification4 = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
-
             System.out.println("收到了自定义消息@@消息内容是:" + content);
             System.out.println("收到了自定义消息@@消息extra是:" + extra);
-            System.out.println("收到了自定义消息@@title:" + title);
-            System.out.println("收到了自定义消息@@status:" + status);
-            System.out.println("收到了自定义消息@@notification:" + notification);
-            System.out.println("收到了自定义消息@@notification2:" + notification2);
-            System.out.println("收到了自定义消息@@notification3:" + notification3);
-            System.out.println("收到了自定义消息@@notification4:" + notification4);
 
-            System.out.println("ddddd" + JPushInterface.ACTION_NOTIFICATION_RECEIVED);
-            System.out.println("ddddd" + JPushInterface.ACTION_NOTIFICATION_OPENED);
-            System.out.println("ddddd" + JPushInterface.ACTION_NOTIFICATION_RECEIVED_PROXY);
-            System.out.println("ddddd" + JPushInterface.ACTION_NOTIFICATION_CLICK_ACTION);
-
-            System.out.println("eee" + JPushInterface.ACTION_NOTIFICATION_CLICK_ACTION);
-            System.out.println("eee" + JPushInterface.ACTION_NOTIFICATION_RECEIVED);
-            System.out.println("eee" + JPushInterface.ACTION_NOTIFICATION_RECEIVED);
 /*
             /*//**************解析推送过来的json数据并存放到集合中 begin******************
             Map<String, Object> map = new HashMap<>();
@@ -119,15 +83,15 @@ public class MyJPushReceiver extends BroadcastReceiver {
             /**
              * 自定义通知
              */
-            PugNotification.with(context)
-                    .load()
-                    .title("医会宝")
-                    .message(content)
-                    .smallIcon(R.mipmap.ic_launcher)//"https://wxt.sinaimg.cn/thumb300/a601622bly1fmzwrjm8cxj20u01e9488.jpg?tags=%5B%5D"
-                    .largeIcon(R.mipmap.ic_launcher)
-                    .flags(Notification.DEFAULT_ALL)
-                    .simple()
-                    .build();
+//            PugNotification.with(context)
+//                    .load()
+//                    .title("医会宝")
+//                    .message(content)
+//                    .smallIcon(R.mipmap.ic_launcher)//"https://wxt.sinaimg.cn/thumb300/a601622bly1fmzwrjm8cxj20u01e9488.jpg?tags=%5B%5D"
+//                    .largeIcon(R.mipmap.ic_launcher)
+//                    .flags(Notification.DEFAULT_ALL)
+//                    .simple()
+//                    .build();
 
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 接收到推送下来的通知");
@@ -139,9 +103,9 @@ public class MyJPushReceiver extends BroadcastReceiver {
             String content = bundle.getString(JPushInterface.EXTRA_ALERT);
             String extra = bundle.getString(JPushInterface.EXTRA_EXTRA);
 
-            System.out.println("收到了自定义消息@@消息标题是:" + title);
-            System.out.println("收到了自定义消息@@消息内容是:" + content);
-            System.out.println("收到了自定义消息@@消息extra是:" + extra);
+            System.out.println("收到了通知@@消息标题是:" + title);
+            System.out.println("收到了通知@@消息内容是:" + content);
+            System.out.println("收到了通知@@消息extra是:" + extra);
 
             JSONObject jsonObject = null;
             try {
@@ -152,21 +116,31 @@ public class MyJPushReceiver extends BroadcastReceiver {
                 e.printStackTrace();
             }
 
-            PugNotification.with(context)
-                    .load()
-                    .title(title)
-                    .message(content)
-                    .smallIcon(R.mipmap.ic_launcher)//"https://wxt.sinaimg.cn/thumb300/a601622bly1fmzwrjm8cxj20u01e9488.jpg?tags=%5B%5D"
-                    .largeIcon(R.mipmap.ic_launcher)
-                    .flags(Notification.DEFAULT_ALL)
-                    .simple()
-                    .build();
+//            PugNotification.with(context)
+//                    .load()
+//                    .title(title)
+//                    .message(content)
+//                    .smallIcon(R.mipmap.ic_launcher)//"https://wxt.sinaimg.cn/thumb300/a601622bly1fmzwrjm8cxj20u01e9488.jpg?tags=%5B%5D"
+//                    .largeIcon(R.mipmap.ic_launcher)
+//                    .flags(Notification.DEFAULT_ALL)
+//                    .simple()
+//                    .build();
 
         } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
 
             // 在这里可以自己写代码去定义用户点击后的行为
-            ToastUtils.show(context, "aaa");
+            String extra = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            ToastUtils.show(context, "aaa "+ extra);
+
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(extra);
+                id = jsonObject.getString("id");
+                type = jsonObject.getString("type");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
             switch (type) {
                 case "active":
@@ -174,28 +148,29 @@ public class MyJPushReceiver extends BroadcastReceiver {
                     break;
                 case "live":
                     intent = new Intent(context, LiveProgramDetailActivity2.class);
-                    intent.putExtra("programId", id);
+                    intent.putExtra("programId", Integer.parseInt(id));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                     break;
                 case "video":
                     intent = new Intent(context, VideoDetailActivity.class);
-                    intent.putExtra("videoId", id);
+                    intent.putExtra("videoId", Integer.parseInt(id));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                     break;
                 case "event":
                     intent = new Intent(context, MeetingDetailActivity.class);
-                    intent.putExtra("eventId", id);
+                    intent.putExtra("eventId", Integer.parseInt(id));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                    break;
+                case "blog":
+                    intent = new Intent(context, NewsActivity.class);
+                    intent.putExtra("blogId", Integer.parseInt(id));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                     break;
             }
-
-
-           /* //打开自定义的Activity
-            Intent i = new Intent(context, TestActivity.class);
-            i.putExtras(bundle);
-            //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP );
-            context.startActivity(i);*/
 
         } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
