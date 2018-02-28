c93d2288aae828288d00baf4a692271d11240b19
luosonglin <iluosonglin@qq.com>
2018-02-28 15:36:05 +0800

    认证权益页

--- app/src/main/java/com/medmeeting/m/zhiyi/UI/IdentityView/RightsActivity.java
+++ app/src/main/java/com/medmeeting/m/zhiyi/UI/IdentityView/RightsActivity.java
@@ -1,31 +1,45 @@
 package com.medmeeting.m.zhiyi.UI.IdentityView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 import com.medmeeting.m.zhiyi.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * 认证权益页
+ */
 public class RightsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.activate)
+    TextView activate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rights);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        toolBar();
     }
 
+    private void toolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.mipmap.back));
+        toolbar.setNavigationOnClickListener(v -> finish());
+    }
+
+    @OnClick(R.id.activate)
+    public void onClick() {
+        startActivity(new Intent(RightsActivity.this, ActivateActivity.class));
+    }
 }
